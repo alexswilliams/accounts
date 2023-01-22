55cb4b7774e3db2a12bdbf4f7a2aee047e3d1ecc
Alex Williams <lexw@live.co.uk>
2023-01-22 17:47:06 +0000

    UI Version bumps

--- 04-ui/src/index.tsx
+++ 04-ui/src/index.tsx
@@ -1,3 +1,8 @@
 import * as React from 'react'
 import * as Server from 'react-dom/server'
+import * as Client from 'react-dom/client'
 
+
+const container = document.getElementById('root')!
+const root = Client.createRoot(container)
+root.render(<h1>Hello React!</h1>)
